c2c15150386194967037d5c02210dd6bb83d3190
justaSalam <salamlex2@gmail.com>
2023-03-15 20:13:42 +0100

    Update LevelLoader.cs

--- raycast rendering/Assets/LevelLoader.cs
+++ raycast rendering/Assets/LevelLoader.cs
@@ -1,9 +1,10 @@
-#if UNITY_EDITOR
+#if UNITY_EDITOR //Dont remove this, game wont build because idk, just keep it there
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+//Ill finish this, maybe..
 public class LevelLoader : EditorWindow
 {
     private Texture2D texture;
